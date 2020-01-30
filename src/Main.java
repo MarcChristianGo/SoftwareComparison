7e95e05ca4d1adabcf87ed971b3385e988262ba5
MarcChristianGo <marc_christian_go@dlsu.edu.ph>
2020-01-30 11:22:12 +0800

    ver 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,51 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[]args) throws IOException{
-        Main check = new Main();
-        check.check("Assets/test_program1.java", "Assets/test_program2.java");
-        check.check("Assets/test_program1.cpp", "Assets/test_program2.cpp");
+    public static void main(String[] args) throws IOException {
+        Main compare = new Main();
+        compare.read("Assets/tocheck");
     }
-
-    void check(String one, String two) throws IOException {
-        double similar = 0, total = 0;
-
-        File fileOne = new File(one);
-        File fileTwo = new File(two);
-
-        BufferedReader readOne = new BufferedReader(new FileReader(fileOne));
-        String lineOne;
-        BufferedReader readTwo = new BufferedReader(new FileReader(fileTwo));
-        String linetwo;
-        lineOne = readOne.readLine();
-        linetwo = readTwo.readLine();
-        while ((lineOne != null) && (linetwo != null)){
-            total ++;
-
-            if (lineOne.equals(linetwo)){
-                similar++;
+    void read(String file){
+        File folder = new File(file);
+        File[] fileList = folder.listFiles();
+        for(int i = 0; i <fileList.length; i++){
+            if(fileList[i].isFile()){
+                System.out.println("File " + fileList[i].getName());
             }
-            lineOne = readOne.readLine();
-            linetwo = readTwo.readLine();
+            else if (fileList[i].isDirectory()) {
+                System.out.println("\nDirectory " + fileList[i].getName());
+                read(file.toString()+"/"+fileList[i].getName());
+            }
         }
-
-        double percentage = 0.0;
-        percentage = similar/total*100;
-        System.out.println("Similarity Score: " + percentage + "%.");
     }
+    void compareNew() throws IOException{
 
+    }
+    void compare(String file1, String file2) throws IOException{
+        File test_program1 = new File(file1);
+        File test_program2 = new File(file2);
+
+        BufferedReader tp1 = new BufferedReader(new FileReader(test_program1));
+        BufferedReader tp2 = new BufferedReader(new FileReader(test_program2));
+
+        String string1=tp1.readLine();
+        String string2=tp2.readLine();
+
+        double total=0;
+        double same=0;
+
+        while (string1!=null){
+            total++;
+            if(string1.equals(string2)){
+                same++;
+            }
+            string1=tp1.readLine();
+            string2=tp2.readLine();
+        }
+        double percent = same/total*100;
+        System.out.println("Java programs are "+percent+" % similar");
+    }
 }
