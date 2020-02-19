f876ef2f2106929b2d1d3c1c2cf155411aa14f01
MarcChristianGo <marc_christian_go@dlsu.edu.ph>
2020-02-20 00:14:51 +0800

    ver 1.3

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,4 @@
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
@@ -16,27 +17,49 @@ public class Matrix {
 
 
     int size;
-    public void initialize() throws IOException {
-        double score;
+
+    public void initialize() throws IOException, IndexOutOfBoundsException {
+        double score = 0;
         Matrix compare = new Matrix();
         ArrayList<String> files = new ArrayList<>();
-        files=compare.read("Assets/tocheck",files);
+        files = compare.read("Assets/GitHubSubmissions", files);
+        String nextName[] = {""};
+        String currentName[];
+        int numberOfFiles = 1;
 
-        for(int m=0; m<files.size();m++)
-        {
+        grid.setAlignment(Pos.CENTER);
+        for (int m = 0; m < files.size(); m++) {
             String name[] = files.get(m).split("/");
-            grid.getColumnConstraints().addAll(new ColumnConstraints(60));
-            grid.add(new Label(name[2]), 0, m+1);
-            grid.add(new Label(name[2]), m+1, 0);
+            grid.getColumnConstraints().addAll(new ColumnConstraints(80));
 
-            for(int h=0;h<files.size();h++){
-                score = compare.compare(files.get(m),files.get(h), m, h);
-                grid.add(new Label(String.valueOf(score)), m+1, h+1);
+            grid.add(new Label(name[2]), 0, m + 1);
+            grid.add(new Label(name[2]), m + 1, 0);
+
+            for (int h = 0; h < files.size(); h++) {
+                if(h+1<files.size()) {
+                    nextName = files.get(h + 1).split("/");
+                }
+
+                score = score + compare.compare(files.get(m), files.get(h), m, h);
+                System.out.println(name[2]+nextName[2]);
+                if (name[2].equals(nextName[2])) {
+                    numberOfFiles++;
+                    System.out.println("same");
+                }
+                else {
+                    System.out.println(numberOfFiles);
+                    score = score / numberOfFiles;
+                    grid.add(new Label(String.valueOf(score)), m + 1, h + 1);
+                    numberOfFiles = 1;
+                    score=0;
+                }
+
             }
         }
 
 
     }
+
     public void setFile(ArrayList<String> file) {
         this.file = file;
     }
@@ -46,46 +69,49 @@ public class Matrix {
     }
 
 
-    ArrayList<String> read(String file,ArrayList<String>files){
+    ArrayList<String> read(String file, ArrayList<String> files) {
         File folder = new File(file);
         File[] fileList = folder.listFiles();
-        for(int i = 0; i <fileList.length; i++){
-            if(fileList[i].isFile()){
-                files.add(file+"/"+fileList[i].getName());
-            }
-            else if (fileList[i].isDirectory()) {
-                read(file.toString()+"/"+fileList[i].getName(),files);
+        for (int i = 0; i < fileList.length; i++) {
+            if (fileList[i].isFile()) {
+                files.add(file + "/" + fileList[i].getName());
+            } else if (fileList[i].isDirectory()) {
+                read(file.toString() + "/" + fileList[i].getName(), files);
             }
         }
         return files;
     }
-    void compareNew() throws IOException{
+
+    void compareNew() throws IOException {
 
 
     }
-    double compare(String file1, String file2, int m, int h) throws IOException{
+
+    double compare(String file1, String file2, int m, int h) throws IOException {
         File test_program1 = new File(file1);
         File test_program2 = new File(file2);
 
         BufferedReader tp1 = new BufferedReader(new FileReader(test_program1));
         BufferedReader tp2 = new BufferedReader(new FileReader(test_program2));
 
-        String string1=tp1.readLine();
-        String string2=tp2.readLine();
+        String string1 = tp1.readLine();
+        String string2 = tp2.readLine();
 
-        double total=0;
-        double same=0;
+        string1 = string1.replaceAll("\\s", "");
+        string2 = string2.replaceAll("\\s", "");
+        double total = 0;
+        double same = 0;
 
-        while (string1!=null){
+        while (string1 != null) {
             total++;
-            if(string1.equals(string2)){
+            if (string1.equals(string2)) {
                 same++;
             }
-            string1=tp1.readLine();
-            string2=tp2.readLine();
+            string1 = tp1.readLine();
+            string2 = tp2.readLine();
         }
-        double percent = same/total*100;
-        percent = Math.round(percent*100.0)/100.0;
+        double percent = same / total;
+        percent = Math.round(percent * 100.0) / 100.0;
 
         return percent;
     }
